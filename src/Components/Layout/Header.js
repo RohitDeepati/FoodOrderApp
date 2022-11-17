71291e0bbb7e2e4dc529a30a0a1a8ead8b21e0ba
rohitdeepati <deepatirohit@gmail.com>
2022-11-17 12:45:05 +0530

    added cart functionality

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImg from "../../assests/meals.jpg";
 import './Header.css';
 
@@ -7,9 +8,9 @@ const Header = (props) => {
     <Fragment>
       <header className="header" >
         <h1>Food Zone</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
       </header>
-      <div className="main-img" >
+      <div className="main-image" >
         <img src={mealsImg} alt="A table full of delicious food!" />
       </div>
     </Fragment>
