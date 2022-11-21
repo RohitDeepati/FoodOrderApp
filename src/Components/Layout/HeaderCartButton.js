da0334016bddc13072985165879c73a2e954d7d5
rohitdeepati <deepatirohit@gmail.com>
2022-11-21 19:33:56 +0530

    fixed css issues

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../store/cart-context";
+import "./HeaderCartButton.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
